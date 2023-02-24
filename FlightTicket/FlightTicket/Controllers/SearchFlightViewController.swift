5cd74802ec872a5edca734292ee0d849a9194324
alex99091 <bkk99091@gmail.com>
2023-02-24 11:41:55 +0900

    FEATURE: Refactoring App Structure and Setting

--- FlightTicket/FlightTicket/Controllers/SearchFlightViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SearchFlightViewController.swift
-//  FlightTicket
-//
-//  Created by BOONGKI KWAK on 2023/01/28.
-//
-
-import Foundation
--- FlightTicket/FlightTicket/Models/FlightInfoEntry.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FlightInfoEntry.swift
-//  FlightTicket
-//
-//  Created by BOONGKI KWAK on 2023/01/28.
-//
-
-import Foundation
